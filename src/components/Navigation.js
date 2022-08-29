9aae83a7bb3b9e97b3608d4943decdac3851e0fe
Sarah HAMI <sarah.hami07@gmail.com>
2022-08-29 11:56:52 +0200

    feature - error page for no matching url

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../views/Home";
 import Product from "../views/Product";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Error from "../views/Error";
 
 const Navigation = () => {
   return (
@@ -12,6 +13,7 @@ const Navigation = () => {
           path="product/:image_url/:product_name/:categories/:ingredients_text/:allergens_imported/:id"
           element={<Product />}
         />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   );
---
+++ src/views/Error.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ButtonHome from "../components/ButtonHome";
+
+function Error() {
+  return (
+    <div className="error__container">
+      <ButtonHome />
+      <img
+        className="img__error"
+        src="https://cdn.pixabay.com/photo/2018/01/16/10/36/mistake-3085712_960_720.jpg"
+        alt="error"
+      />
+    </div>
+  );
+}
+
+export default Error;
