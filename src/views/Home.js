0cbccaeb6bd8449e4c46d5a9656ee71f313cf9f4
Sarah HAMI <sarah.hami07@gmail.com>
2022-08-29 16:16:49 +0200

    refacto - Search Input - remove useState and replace by useSearchParams()

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,23 +1,28 @@
-import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import Loader from "../components/Loader";
 import ProductCard from "../components/ProductCard";
 import SearchInput from "../components/SearchInput";
 import useProductsList from "../utils/useProductList";
 
 const Home = () => {
-  const [query, setQuery] = useState("");
   const { data, isLoading } = useProductsList();
+  const [search, setSearch] = useSearchParams();
 
   const handleChange = (e) => {
     e.preventDefault();
     const searchedWord = e.target.value;
-    setQuery(searchedWord);
+    setSearch({ product_name: searchedWord });
   };
+
+  const searchByProductName = (product_name) => (product) =>
+    product.product_name
+      .toLowerCase()
+      .includes((product_name || "").toLowerCase());
 
   return (
     <>
       <SearchInput
-        value={query}
+        value={search.get("product_name")}
         handleChange={handleChange}
         placeholder="Type your search..."
       />
@@ -25,25 +30,21 @@ const Home = () => {
       {isLoading ? (
         <Loader />
       ) : (
-        <div className="products__list__container">
+        <div className="products__list__container" data-testid="productList">
           {data.products
-            .filter((product) =>
-              product.product_name.toLowerCase().includes(query.toLowerCase())
-            )
+            .filter(searchByProductName(search.get("product_name")))
             .map((product) => {
               return (
-                <>
-                  <ProductCard
-                    key={product._id}
-                    productName={product.product_name}
-                    productImgCard={product.image_front_small_url}
-                    productId={product._id}
-                    productCategories={product.categories}
-                    productIngredientsText={product.ingredients_text}
-                    productAllergensImported={product.allergens_imported}
-                    imageUrl={product.image_front_url}
-                  />
-                </>
+                <ProductCard
+                  key={product._id}
+                  productName={product.product_name}
+                  productImgCard={product.image_front_small_url}
+                  productId={product._id}
+                  productCategories={product.categories}
+                  productIngredientsText={product.ingredients_text}
+                  productAllergensImported={product.allergens_imported}
+                  imageUrl={product.image_front_url}
+                />
               );
             })}
         </div>
